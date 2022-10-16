8efb7ce649c1d75fc34c63ed25658b31f4b964d2
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-10-16 12:04:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://tnlink.in/api'
+    url = f'https://pdiskshortener.com/api'
     params = {'api': 'b6aace46d40c605fff8e0cafbcd8fbe416851f4d',
               'url': link,
               }
